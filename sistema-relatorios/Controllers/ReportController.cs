bdf9498f23f856d0569bce6b20ffb8dc820cbad0
Jackson de Almeida <jackson.almeida@luz.intra>
2024-03-17 22:47:37 -0300

    Subindo projeto

---
+++ sistema-relatorios/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sistema_relatorios.database;
+
+namespace sistema_relatorios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+    }
+}
---
+++ sistema-relatorios/models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace sistema_relatorios.models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; } // ? informa que a string pode vir nula.
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
